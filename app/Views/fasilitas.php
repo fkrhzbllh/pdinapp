cb47dc2c9d72c052c4bcc18f16325e565b1090b6
fkrhzbllh <flaminggooo50@gmail.com>
2023-05-08 20:03:46 +0700

    fix filter and search

--- app/Views/fasilitas.php
+++ app/Views/fasilitas.php
@@ -12,13 +12,16 @@
                         <button class="nav-link" id="pills-alat-tab" data-bs-toggle="pill" data-bs-target="#pills-alat" type="button" role="tab" aria-controls="pills-alat" aria-selected="false">Alat</button>
                     </li>
                 </ul>
-                <select class="form-select ms-auto" aria-label="Default select example" id="filter" style="width: fit-content;height: fit-content;">
-                    <option selected value="all">Pilih tipe ruangan</option>
-                    <option value="Pameran">Pameran</option>
-                    <option value="Kantor">Kantor</option>
-                    <option value="Meeting">Ruang Pertemuan</option>
-                    <option value="Lainnya">Lainnya</option>
-                </select>
+                <div class="input-group ms-auto" style="width: fit-content;height: fit-content;" id="input-group">
+                    <span class="input-group-text">Filter</span>
+                    <select class="form-select ms-auto" aria-label="Default select example" id="filter" style="width: fit-content;height: fit-content;">
+                        <option selected value="all">Semua</option>
+                        <option value="Pameran">Pameran</option>
+                        <option value="Kantor">Kantor</option>
+                        <option value="Meeting">Ruang Pertemuan</option>
+                        <option value="Lainnya">Lainnya</option>
+                    </select>
+                </div>
                 <input type="text" class="form-control ms-3" style="height: fit-content; width:fit-content;" id="cariruangan" placeholder="Cari ruangan...">
                 <input type="text" class="form-control ms-auto" style="height: fit-content; width:fit-content; display:none;" id="carialat" placeholder="Cari alat...">
             </div>
@@ -27,8 +30,8 @@
                 <div class="tab-pane fade show active" id="pills-ruangan" role="tabpanel" aria-labelledby="pills-ruangan-tab" tabindex="0">
                     <div class="row justify-content-start mb-5">
                         <?php foreach($ruangan as $r) : ?>
-                        <div class="col-4 <?= $r['tipe']?>">
-                            <div class="card" >
+                        <div class="col-4 <?= $r['tipe']?> mb-3">
+                            <div class="card">
                                 <img class="card-img-top" src="<?= base_url()?>uploads/5ea50f43831c2.png">
                             <!-- <h5 class="card-header">Featured</h5> -->
                                 <div class="card-body">
@@ -65,39 +68,68 @@
     </div>
 </div>
 
+<!-- fitur cari ruangan dan alat -->
 <script>
     $(document).ready(function(){
         $("#cariruangan").on("keyup", function() {
-            var value = $(this).val().toLowerCase();
-            $("#pills-tabContent").children().children().children().filter(function() {
-                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            var keyword = $(this).val().toLowerCase();
+            $("#pills-ruangan").children().children().filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(keyword) > -1)
             });
         });
         $("#carialat").on("keyup", function() {
-            var value = $(this).val().toLowerCase();
-            $("#pills-tabContent").children().children().children().filter(function() {
-                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            var keyword = $(this).val().toLowerCase();
+            $("#pills-alat").children().children().filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(keyword) > -1)
             });
         });
     });
 </script>
 
+<!-- toggle kolom filter dan cari -->
 <script>
     $(document).ready(function(){
         $('#pills-tab').find('button').on('show.bs.tab',function() {
-            $('#filter').toggle();
+            $('#input-group').toggle();
             $('#cariruangan').toggle();
             $('#carialat').toggle();
         });
     });
 </script>
 
+<!-- fitur filter ruangan -->
 <script>
     $(document).ready(function(){
         $("#filter").on("change", function() {
             var value = $(this).val();
-            $("#pills-tabContent").children().children().children().filter(function() {
-                $(this).toggle($(this).hasClass(value))
+            $("#pills-ruangan").children().children().filter(function() {
+                if (value == 'all'){
+                    $("#cariruangan").on("keyup", function() {
+                        var keyword = $(this).val().toLowerCase();
+                        $("#pills-ruangan").children().children().filter(function() {
+                            $(this).toggle($(this).text().toLowerCase().indexOf(keyword) > -1);
+                        });
+                    });
+                    $(this).show();
+
+                    var keyword = $("#cariruangan").val().toLowerCase();
+                        $("#pills-ruangan").children().children().filter(function() {
+                            $(this).toggle($(this).text().toLowerCase().indexOf(keyword) > -1);
+                        });
+                } else {
+                    $("#cariruangan").on("keyup", function() {
+                        var keyword = $(this).val().toLowerCase();
+                        $("#pills-ruangan").children().children().filter(function() {
+                            $(this).toggle($(this).text().toLowerCase().indexOf(keyword) > -1 && $(this).hasClass(value));
+                        });
+                    });
+
+                    var keyword = $("#cariruangan").val().toLowerCase();
+                        $("#pills-ruangan").children().children().filter(function() {
+                            $(this).toggle($(this).text().toLowerCase().indexOf(keyword) > -1 && $(this).hasClass(value));
+                        });
+                    $(this).toggle($(this).hasClass(value));   
+                }
             });
         });
     });
