dad508cd11be19d89f040c4bff4430830fe88b43
Damar Maulana <ibn.damr@gmail.com>
2023-12-04 20:15:11 +0700

    Updated and cleaned up auth

--- app/Views/Shield/login.php
+++ app/Views/Shield/login.php
@@ -1,80 +1,83 @@
 <?= $this->extend('layout/auth/template') ?>
 
+<?= $this->section('title') ?>
+<?= lang('Auth.login') ?>
+<?= $this->endSection() ?>
+
 <?= $this->section('content') ?>
-<!-- <section id="section-konten"> -->
-<div class="container d-flex justify-content-center p-5">
-    <div class="container-konten col-12 col-md-8 col-xl-5">
-        <div class="card-body">
-            <h5 class="card-title mb-5"><?= lang('Auth.login') ?></h5>
 
-            <?php if (session('error') !== null) : ?>
-                <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
-            <?php elseif (session('errors') !== null) : ?>
-                <div class="alert alert-danger" role="alert">
-                    <?php if (is_array(session('errors'))) : ?>
-                        <?php foreach (session('errors') as $error) : ?>
-                            <?= $error ?>
-                            <br>
-                        <?php endforeach ?>
-                    <?php else : ?>
-                        <?= session('errors') ?>
-                    <?php endif ?>
-                </div>
-            <?php endif ?>
-
-            <?php if (session('message') !== null) : ?>
-                <div class="alert alert-success" role="alert"><?= session('message') ?></div>
-            <?php endif ?>
-
-            <form action="<?= url_to('login') ?>" method="post">
-                <?= csrf_field() ?>
-
-                <!-- Email -->
-                <div class="mb-2">
-                    <input type="email" class="form-control" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required />
-                </div>
-
-                <!-- Password -->
-                <div class="mb-2">
-                    <input type="password" class="form-control" name="password" inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>" required />
-                </div>
-
-                <!-- Remember me -->
-                <?php if (setting('Auth.sessionConfig')['allowRemembering']) : ?>
-                    <div class="form-check">
-                        <label class="form-check-label">
-                            <input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')) : ?> checked<?php endif ?>>
-                            <?= lang('Auth.rememberMe') ?>
-                        </label>
-                    </div>
-                <?php endif; ?>
-
-                <button type="submit" class="btn btn-danger w-100 mt-2 mb-2"><?= lang('Auth.login') ?></button>
-
-                <?php if (setting('Auth.allowMagicLinkLogins')) : ?>
-                    <p class="text-center"><?= lang('Auth.forgotPassword') ?> <a href="<?= url_to('magic-link') ?>"><?= lang('Auth.useMagicLink') ?></a></p>
-                <?php endif ?>
-
-            </form>
-            <div class="row mt-4 mb-2">
-                <button type="button" class="btn btn-outline-dark w-100 mb-2">
-                    <i class="bi bi-google me-2"></i>
-                    Masuk dengan Google
-                </button>
-                <button type="button" class="btn btn-outline-primary w-100">
-                    <i class="bi bi-github me-2"></i>
-                    Masuk dengan GitHub
-                </button>
-            </div>
-            <div class="row">
-                <?php if (setting('Auth.allowRegistration')) : ?>
-                    <p class="text-center"><?= lang('Auth.needAccount') ?> <a href="<?= url_to('register') ?>"><?= lang('Auth.register') ?></a></p>
-                <?php endif ?>
-            </div>
-        </div>
+<!-- Pesan error -->
+<?php if (session('error') !== null) : ?>
+    <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+<?php elseif (session('errors') !== null) : ?>
+    <div class="alert alert-danger" role="alert">
+        <?php if (is_array(session('errors'))) : ?>
+            <?php foreach (session('errors') as $error) : ?>
+                <?= $error ?>
+                <br>
+            <?php endforeach ?>
+        <?php else : ?>
+            <?= session('errors') ?>
+        <?php endif ?>
     </div>
-</div>
-<!-- </section> -->
+<?php endif ?>
+
+<!-- Pesan sukses -->
+<?php if (session('message') !== null) : ?>
+    <div class="alert alert-success" role="alert"><?= session('message') ?></div>
+<?php endif ?>
+
+<!-- Form input login -->
+<form action="<?= url_to('login') ?>" method="post">
+    <?= csrf_field() ?>
+
+    <!-- Email -->
+    <div class="mb-2">
+        <input type="email" class="form-control" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required />
+    </div>
+
+    <!-- Password -->
+    <div class="mb-2">
+        <input type="password" class="form-control" name="password" inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>" required />
+    </div>
+
+    <!-- Remember me -->
+    <?php if (setting('Auth.sessionConfig')['allowRemembering']) : ?>
+        <div class="form-check">
+            <label class="form-check-label">
+                <input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')) : ?> checked<?php endif ?>>
+                <?= lang('Auth.rememberMe') ?>
+            </label>
+        </div>
+    <?php endif; ?>
+
+    <button type="submit" class="btn btn-danger w-100 mt-2 mb-2"><?= lang('Auth.login') ?></button>
+
+    <?php if (setting('Auth.allowMagicLinkLogins')) : ?>
+        <p class="text-center"><?= lang('Auth.forgotPassword') ?> <a href="<?= url_to('magic-link') ?>"><?= lang('Auth.useMagicLink') ?></a></p>
+    <?php endif ?>
+
+</form>
+
+<!-- Opsi masuk dengan -->
+<a href="<?= base_url('oauth/google') ?>">
+    <button type="button" class="btn btn-outline-dark w-100 mb-2">
+        <i class="bi bi-google me-2"></i>
+        Masuk dengan Google
+    </button>
+</a>
+<a href="<?= base_url('oauth/github') ?>">
+    <button type="button" class="btn btn-outline-primary w-100">
+        <i class="bi bi-github me-2"></i>
+        Masuk dengan GitHub
+    </button>
+</a>
+
+<!-- Link daftar -->
+<?php if (setting('Auth.allowRegistration')) : ?>
+    <p class="text-center mt-2"><?= lang('Auth.needAccount') ?> <a href="<?= url_to('register') ?>"><?= lang('Auth.register') ?></a></p>
+<?php endif ?>
+
 <?= $this->endSection() ?>
 
 <?= $this->section('script') ?>
--- app/Views/Shield/register.php
+++ app/Views/Shield/register.php
@@ -1,62 +1,67 @@
-<?= $this->extend(config('Auth')->views['layout']) ?>
+<?= $this->extend('layout/auth/template') ?>
 
-<?= $this->section('title') ?><?= lang('Auth.register') ?> <?= $this->endSection() ?>
+<?= $this->section('title') ?>
+<?= lang('Auth.register') ?>
+<?= $this->endSection() ?>
 
-<?= $this->section('main') ?>
-{{ShieldOAuthButtonForRegisterPage}}
+<?= $this->section('content') ?>
 
-<div class="container d-flex justify-content-center p-5">
-    <div class="card col-12 col-md-5 shadow-sm">
-        <div class="card-body">
-            <h5 class="card-title mb-5"><?= lang('Auth.register') ?></h5>
-
-            <?php if (session('error') !== null) : ?>
-                <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
-            <?php elseif (session('errors') !== null) : ?>
-                <div class="alert alert-danger" role="alert">
-                    <?php if (is_array(session('errors'))) : ?>
-                        <?php foreach (session('errors') as $error) : ?>
-                            <?= $error ?>
-                            <br>
-                        <?php endforeach ?>
-                    <?php else : ?>
-                        <?= session('errors') ?>
-                    <?php endif ?>
-                </div>
-            <?php endif ?>
-
-            <form action="<?= url_to('register') ?>" method="post">
-                <?= csrf_field() ?>
-
-                <!-- Email -->
-                <div class="mb-2">
-                    <input type="email" class="form-control" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required />
-                </div>
-
-                <!-- Username -->
-                <div class="mb-4">
-                    <input type="text" class="form-control" name="username" inputmode="text" autocomplete="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>" required />
-                </div>
-
-                <!-- Password -->
-                <div class="mb-2">
-                    <input type="password" class="form-control" name="password" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.password') ?>" required />
-                </div>
-
-                <!-- Password (Again) -->
-                <div class="mb-5">
-                    <input type="password" class="form-control" name="password_confirm" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.passwordConfirm') ?>" required />
-                </div>
-
-                <div class="d-grid col-12 col-md-8 mx-auto m-3">
-                    <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.register') ?></button>
-                </div>
-
-                <p class="text-center"><?= lang('Auth.haveAccount') ?> <a href="<?= url_to('login') ?>"><?= lang('Auth.login') ?></a></p>
-
-            </form>
-        </div>
+<!-- Pesan error -->
+<?php if (session('error') !== null) : ?>
+    <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+<?php elseif (session('errors') !== null) : ?>
+    <div class="alert alert-danger" role="alert">
+        <?php if (is_array(session('errors'))) : ?>
+            <?php foreach (session('errors') as $error) : ?>
+                <?= $error ?>
+                <br>
+            <?php endforeach ?>
+        <?php else : ?>
+            <?= session('errors') ?>
+        <?php endif ?>
     </div>
-</div>
+<?php endif ?>
 
+<form action="<?= url_to('register') ?>" method="post">
+    <?= csrf_field() ?>
+
+    <!-- Email -->
+    <div class="mb-2">
+        <input type="email" class="form-control" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required />
+    </div>
+
+    <!-- Username -->
+    <div class="mb-4">
+        <input type="text" class="form-control" name="username" inputmode="text" autocomplete="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>" required />
+    </div>
+
+    <!-- Password -->
+    <div class="mb-2">
+        <input type="password" class="form-control" name="password" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.password') ?>" required />
+    </div>
+
+    <!-- Password (Again) -->
+    <div class="mb-4">
+        <input type="password" class="form-control" name="password_confirm" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.passwordConfirm') ?>" required />
+    </div>
+
+    <button type="submit" class="btn btn-danger w-100 mt-2 mb-2"><?= lang('Auth.register') ?></button>
+
+    <p class="text-center"><?= lang('Auth.haveAccount') ?> <a href="<?= url_to('login') ?>"><?= lang('Auth.login') ?></a></p>
+
+</form>
+
+<a href="<?= base_url('oauth/google') ?>">
+    <button type="button" class="btn btn-outline-dark w-100 mb-2">
+        <i class="bi bi-google me-2"></i>
+        Masuk dengan Google
+    </button>
+</a>
+
+<a href="<?= base_url('oauth/github') ?>">
+    <button type="button" class="btn btn-outline-primary w-100">
+        <i class="bi bi-github me-2"></i>
+        Masuk dengan GitHub
+    </button>
+</a>
 <?= $this->endSection() ?>
